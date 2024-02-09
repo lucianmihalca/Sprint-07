6ba0a87794b3a1872132675428a3cef23c8c2674
LucianMihalca <infolucianmihalca@gmail.com>
2024-02-10 00:05:29 +0100

    Implementado el controlador de logout

--- server/src/controllers/auth.controller.ts
+++ server/src/controllers/auth.controller.ts
@@ -64,7 +64,7 @@ export const login = async (req: Request, res: Response) => {
       return res.status(400).json({ error: 'Invalid credentials' });
     }
     generateTokenAndSetCookie(user._id, res);
-    
+
     res.status(200).json({
       _id: user._id,
       fullName: user.fullName,
@@ -79,6 +79,13 @@ export const login = async (req: Request, res: Response) => {
   }
 };
 export const logout = async (req: Request, res: Response) => {
-  console.log('logoutUser');
-  res.send('logoutUser');
+  try {
+    res.cookie('jwt', '', { maxAge: 0 });
+    res.status(200).json({ message: 'Logged out successfully' });
+  } catch (error) {
+    if (error instanceof Error) {
+      console.log('Error in logout controller', error.message);
+      res.status(500).json({ error: 'Internal server error' });
+    }
+  }
 };
