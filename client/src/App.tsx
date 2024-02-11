daa76a45125c06875fe20d1bf6e2a61483683989
LucianMihalca <infolucianmihalca@gmail.com>
2024-02-11 20:08:46 +0100

    Se implementa el componente SignUp y GenderCheckBox - Se ajustan estilos y funcionalidad de SignUp

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,13 @@
 import './App.css';
-import Login from './pages/login/Login';
+
+import SignUp from './pages/signup/SignUp';
 
 function App() {
-  return <div className='p-4 h-screen flex items-center justify-center'>
-    
-  <Login />
-
-  </div>;
+  return (
+    <div className="p-4 h-screen flex items-center justify-center">
+      <SignUp />
+    </div>
+  );
 }
 
 export default App;
